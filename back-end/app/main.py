b9f9f585a048572173ef8d91ec795d0159f3ef6f
Vladimir Luzin <vvluzin2004@gmail.com>
2023-10-27 18:25:23 +0500

    CORS

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from random import randint
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 trash = {"glass": randint(0, 10), "wood": randint(0, 10), "metal": randint(0, 10), "plastic": randint(0, 10)}
 
